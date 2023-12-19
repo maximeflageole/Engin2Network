da80fd15f0ad1280e3bbf5a2e237f458a9d78d22
maximeflageole <maximeflageole@gmail.com>
2023-12-19 14:58:09 -0500

    Adding humanoid asset Creating a NetworkedMainCharacter script with navmesh movements Set up a networked scene Feeding the animator parameters

---
+++ NetworkExample/Assets/Scripts/NetworkedCharacterController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Mirror;
+
+public class NetworkedCharacterController : NetworkBehaviour
+{
+    private const float MINIMUM_MOVEMENT_SPEED = 0.01f;
+    [SerializeField]
+    private NavMeshAgent m_navmeshAgent;
+    [SerializeField]
+    private Animator m_animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float currentSpeed = m_navmeshAgent.velocity.magnitude;
+        m_animator.SetBool("Moving", currentSpeed > MINIMUM_MOVEMENT_SPEED);
+        m_animator.SetFloat("Velocity Z", currentSpeed / m_navmeshAgent.speed);
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            int layerMask = LayerMask.NameToLayer("Navmesh");
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~layerMask))
+            {
+                m_navmeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
